c9a404232f6224414848b5c9ea11d0486988aad1
iliaskaras <hlias.karas@yahoo.gr>
2018-10-16 23:45:24 +0300

    added phone number validator

--- src/main/java/com/karatsin/onlinemoviestore/validation/constraints/PhoneNumberValidator.java
+++ src/main/java/com/karatsin/onlinemoviestore/validation/constraints/PhoneNumberValidator.java
@@ -11,8 +11,8 @@ ConstraintValidator<PhoneNumberConstraint, String> {
   }
 
   @Override
-  public boolean isValid(String contactField,
-    ConstraintValidatorContext cxt) {
+  public boolean isValid(String contactField,ConstraintValidatorContext cxt) {
+	  
       return contactField != null && contactField.matches("[0-9]+")
         && (contactField.length() > 8) && (contactField.length() < 14);
   }
