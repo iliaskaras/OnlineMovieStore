b905f323e6f53c889def50ad2bd904d9bbb8c7bb
iliaskaras <hlias.karas@yahoo.gr>
2018-10-20 23:13:29 +0300

    added interface for movie exception handler

--- src/main/java/com/karatsin/onlinemoviestore/rest/controller/exception/movie/IMovieExceptionHandler.java
+++ src/main/java/com/karatsin/onlinemoviestore/rest/controller/exception/movie/IMovieExceptionHandler.java
@@ -3,6 +3,7 @@ package com.karatsin.onlinemoviestore.rest.controller.exception.movie;
 import org.springframework.http.ResponseEntity;
 
 import com.karatsin.onlinemoviestore.rest.controller.exception.PaymentMethodException;
+import com.karatsin.onlinemoviestore.rest.controller.exception.customer.CustomerWithEmailExistException;
 import com.karatsin.onlinemoviestore.rest.response.IErrorResponse;
 
 public interface IMovieExceptionHandler {
