9d6256cbd631899a869d85175a9c286db73bf13c
iliaskaras <hlias.karas@yahoo.gr>
2018-10-22 22:47:38 +0300

    added RentalPackageDAO and interface

---
+++ src/main/java/com/karatsin/onlinemoviestore/dao/IRentalPackageDAO.java
@@ -0,0 +1,9 @@
+package com.karatsin.onlinemoviestore.dao;
+
+import com.karatsin.onlinemoviestore.entity.RentalPackage;
+
+public interface IRentalPackageDAO {
+
+	public RentalPackage getRentalPackageById(int theRentalPackageId);
+	
+}
